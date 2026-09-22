efb162ad644f24504b107f0fcb4982a5080dd587
hemminwin <103852850+hemminwin@users.noreply.github.com>
2026-09-22 11:21:53 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // PEGA AQUÍ la configuración que te da Firebase al registrar una app Web.
 // No cambies los nombres de las propiedades.
 export const firebaseConfig = {
-  apiKey: "PEGA_AQUI",
-  authDomain: "PEGA_AQUI",
-  databaseURL: "PEGA_AQUI",
-  projectId: "PEGA_AQUI",
-  storageBucket: "PEGA_AQUI",
-  messagingSenderId: "PEGA_AQUI",
-  appId: "PEGA_AQUI"
+  apiKey: ""AIzaSyC86rm0aOvsh7Hj7HW8qTgnLugzoU44Qb4"",
+  authDomain: "custompizarra.firebaseapp.com",
+  databaseURL: "https://custompizarra-default-rtdb.firebaseio.com",
+  projectId: "custompizarra",
+  storageBucket: "custompizarra.firebasestorage.app",
+  messagingSenderId: "372101362076",
+  appId: "1:372101362076:web:dca8b28cb5232ec0bd5859"
 };
